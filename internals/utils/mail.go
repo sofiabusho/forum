761956e819e8d750a30d8741a56cd2830e97c1b1
ttarara <theocharoula.tarara@gmail.com>
2025-07-24 01:42:15 +0300

    Forgot Pass Work

--- internals/utils/mail.go
+++ internals/utils/mail.go
@@ -7,20 +7,22 @@ import (
 
 func SendResetEmail(toEmail, token string) error {
 	from := "plant.talk2025@gmail.com" // ➤ βάλε εδώ το Gmail σου
-	password := "niicnftnethvawxf"     // ✅ χωρίς κενά
+	password := "niicnftnethvawxf"     // ✅ Gmail App Password
 	smtpHost := "smtp.gmail.com"
 	smtpPort := "587"
 
-	// Φτιάχνουμε το reset link
-	resetLink := fmt.Sprintf("http://localhost:8080/reset-password?token=%s", token)
+	// ✅ Σωστό reset link προς add-newpassword.html
+	resetLink := fmt.Sprintf("http://localhost:8080/add-newpassword.html?token=%s", token)
 
-	// Μήνυμα
-	message := []byte(fmt.Sprintf("Subject: Reset your password\n\nClick the link below to reset your password:\n%s", resetLink))
+	// Μήνυμα email
+	message := []byte(fmt.Sprintf(
+		"Subject: Reset your password\n\nClick the link below to reset your password:\n%s", resetLink,
+	))
 
-	// Auth object
+	// SMTP Auth
 	auth := smtp.PlainAuth("", from, password, smtpHost)
 
-	// Αποστολή
+	// Αποστολή email
 	err := smtp.SendMail(smtpHost+":"+smtpPort, auth, from, []string{toEmail}, message)
 	if err != nil {
 		return err
