7d9a09330a7e78c5c1b7a1d8bbb286ee0fbb3158
ttarara <theocharoula.tarara@gmail.com>
2025-08-29 16:50:00 +0300

    build.sh

---
+++ build.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+# build.sh - build and run the forum project with Docker
+
+# Stop and remove old containers/networks/volumes
+docker compose down -v
+
+# Build and start the new container in detached mode
+docker compose up -d --build
+
+# List all Docker images
+docker images   
+
+# Show running containers
+docker compose ps
